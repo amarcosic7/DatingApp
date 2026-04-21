cafb45237d1b3fcfbf1eb60b324c3e919d923b82
amarcosic7 <amarcosic2016@gmail.com>
2026-04-21 18:22:32 +0200

    feat: add UserDto and update AccountController to return JWT token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -5,15 +5,17 @@ using System.Windows.Markup;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(AppDbContext context) : BaseApiController
+public class AccountController(AppDbContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] //api/account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if(await EmailExists(registerDto.Email)) return BadRequest("Email taken!");
 
@@ -29,25 +31,28 @@ public class AccountController(AppDbContext context) : BaseApiController
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
-        return user;
+        
+        return user.ToDto(tokenService);
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.SingleOrDefaultAsync(x=>x.Email == loginDto.Email);
 
         if(user == null) return Unauthorized("Invalid email address!");
         
-        using var hmac = new HMACSHA512();
+        using var hmac = new HMACSHA512(user.PasswordSalt);
 
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Email));
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
         for (var i = 0; i < computedHash.Length; i++)
         {
             if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password!");
         }
-        return user;
+        
+        return user.ToDto(tokenService);
+
     }
 
     private async Task<bool> EmailExists(string email)
