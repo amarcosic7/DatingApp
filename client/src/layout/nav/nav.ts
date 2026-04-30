90415fbd4756f95e391122dc8196b3b48617274a
amarcosic7 <amarcosic2016@gmail.com>
2026-04-30 21:26:33 +0200

    feat: implement login and logout logic in nav

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,4 +1,4 @@
-import { Component, inject, resource } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
 
@@ -13,11 +13,25 @@ import { AccountService } from '../../core/services/account-service';
 export class Nav {
   private accountService = inject(AccountService);
   protected creds: any = {}
+  protected loggedIn = signal(false);
+
 
   login(){
     this.accountService.login(this.creds).subscribe({
-      next: result => console.log(result),
+      next: result => {
+      console.log(result),
+      this.loggedIn.set(true),
+      this.creds = {};
+      },  
       error: error => alert(error.message)
     })
   }
+
+  logout(){
+    this.loggedIn.set(false);
+  }
+
+
 }
+
+
