55381062c854969f03c4c42e0aa2fcff0d6363b2
amarcosic7 <amarcosic2016@gmail.com>
2026-05-01 18:31:44 +0200

    feat: connect nav component to account service for login/logout

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -11,16 +11,14 @@ import { AccountService } from '../../core/services/account-service';
   styleUrl: './nav.css',
 })
 export class Nav {
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   protected creds: any = {}
-  protected loggedIn = signal(false);
 
 
   login(){
     this.accountService.login(this.creds).subscribe({
       next: result => {
       console.log(result),
-      this.loggedIn.set(true),
       this.creds = {};
       },  
       error: error => alert(error.message)
@@ -28,7 +26,7 @@ export class Nav {
   }
 
   logout(){
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 
 
