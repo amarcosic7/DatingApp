de8d08da27edfe56d8851d0eff6dd8587ee70fb9
amarcosic7 <amarcosic2016@gmail.com>
2026-05-07 18:24:32 +0200

    feat: add navigation links and active route styling

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,24 +1,26 @@
 import { Component, inject, resource, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
 
 
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountService = inject(AccountService);
   protected creds: any = {}
+  private router = inject(Router);
 
 
   login(){
     this.accountService.login(this.creds).subscribe({
       next: result => {
-      console.log(result),
+      this.router.navigateByUrl('/members'),
       this.creds = {};
       },  
       error: error => alert(error.message)
@@ -27,6 +29,7 @@ export class Nav {
 
   logout(){
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 
