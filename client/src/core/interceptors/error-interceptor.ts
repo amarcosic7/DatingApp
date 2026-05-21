859ea594d497425f68ae7ee84262c3c02f09ab8c
amarcosic7 <amarcosic2016@gmail.com>
2026-05-21 18:39:05 +0200

    feat: add server-error component and handle 500 internal server errors

--- client/src/core/interceptors/error-interceptor.ts
+++ client/src/core/interceptors/error-interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { ToastService } from '../services/toast-service';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { catchError } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
@@ -33,7 +33,8 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
            router.navigateByUrl('/not-found');
             break;
           case 500:
-            toast.error('Server error');
+            const navigationExtras: NavigationExtras = {state: {error: error.error}};
+            router.navigateByUrl('/server-error',navigationExtras)
             break;
           default:
             toast.error('Something went wrong');
