7800095180fa84e13a5991508f516ecd0709f5f9
amarcosic7 <amarcosic2016@gmail.com>
2026-05-30 13:16:50 +0200

    feat: create member service and set up development and production environments

---
+++ client/src/core/services/member-service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Member } from '../../types/member';
+import { AccountService } from './account-service';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MemberService {
+
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl;
+  private accountServer = inject(AccountService);
+
+  getMembers(){
+    return this.http.get<Member[]>(this.baseUrl + 'members', this.getHttpOptions());
+  }
+
+    getMember(id: string){
+      return this.http.get<Member>(this.baseUrl + 'member/' + id,this.getHttpOptions());
+    }
+
+
+    private getHttpOptions(){
+      return{
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + this.accountServer.currentUser()?.token
+        })
+      }
+    }
+}
