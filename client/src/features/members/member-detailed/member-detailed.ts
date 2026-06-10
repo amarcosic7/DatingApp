a2d79cb11156327f1d001cfa1597a2e2f5bf5324
amarcosic7 <amarcosic2016@gmail.com>
2026-06-10 20:45:55 +0200

    feat: implement member resolver to pre-fetch data before routing

--- client/src/features/members/member-detailed/member-detailed.ts
+++ client/src/features/members/member-detailed/member-detailed.ts
@@ -16,12 +16,14 @@ export class MemberDetailed implements OnInit{
   private memberService = inject(MemberService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
+  protected member = signal<Member | undefined>(undefined);
   protected title = signal<string | undefined>('Profile');
 
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+      next: data => this.member.set(data['member'])
+    })
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
@@ -31,11 +33,5 @@ export class MemberDetailed implements OnInit{
         this.title.set(this.route.firstChild?.snapshot?.title);
       }
     })
-  }
-
-  loadMember(){
-    const id = this.route.snapshot.paramMap.get('id');
-    if(!id) return;
-    return this.memberService.getMember(id);
   }
 }
