702b07f49918a4855adfb66196461dc57012dc5a
amarcosic7 <amarcosic2016@gmail.com>
2026-06-25 17:35:44 +0200

    feat: use ClaimsPrincipal extension to handle member profile update

---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using System.Security.Claims;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace API.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static string GetMemberId(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier) ?? 
+            throw new Exception("Cannot get memberId from token");
+    }
+}
