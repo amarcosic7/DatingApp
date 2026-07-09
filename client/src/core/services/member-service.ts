9ed0a7bf183b31f198521fa85fa12083a54bf987
amarcosic7 <amarcosic2016@gmail.com>
2026-07-09 13:04:19 +0200

    feat: add image upload component and drag-and-drop layout

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -13,7 +13,7 @@ export class MemberService {
 
   private http = inject(HttpClient);
   private baseUrl = environment.apiUrl;
-  editMode = signal(false);
+  editMode = signal(true);
   member = signal<Member | null>(null);
 
 
