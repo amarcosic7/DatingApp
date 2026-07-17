51ccf073dc1506fc027122447f0c186ae24c7592
amarcosic7 <amarcosic2016@gmail.com>
2026-07-17 14:11:59 +0200

    feat: deleting a photo from API

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
@@ -104,6 +105,32 @@ namespace API.Controllers
             if(await memberRepository.SaveAllAsync()) return NoContent();
 
             return BadRequest("Problem setting main photo");
+        }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId)        
+        {
+            var member = await memberRepository.GetMemberForUpdate(User.GetMemberId());
+
+            if(member == null) return BadRequest("Cannot get member from token");
+
+            var photo = member.Photos.SingleOrDefault(x=>x.Id == photoId);
+
+            if(photo == null || photo.Url == member.ImageUrl)
+            {
+                return BadRequest("This photo cannot be deleted");
+            }
+
+            if(photo.PublicID != null)
+            {
+                var result = await photoService.DeletePhotoAsync(photo.PublicID);
+                if(result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            member.Photos.Remove(photo);
+            if(await memberRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting the photo");
         }
     }
 }
