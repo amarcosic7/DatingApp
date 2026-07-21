734dd8204e9f04eb9d5324c8785355c800a27470
amarcosic7 <amarcosic2016@gmail.com>
2026-07-21 12:49:05 +0200

    feat: add credentials step to registration form

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, output } from '@angular/core';
+import { Component, inject, OnInit, output, signal } from '@angular/core';
 import { RegisterCreds } from '../../../types/user';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
@@ -18,17 +18,26 @@ export class Register{
   private fb = inject(FormBuilder);
   cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
-  protected registerForm : FormGroup;
+  protected credentialsForm : FormGroup;
+  protected profileForm : FormGroup;
+  protected currentStep = signal(1);
 
   constructor(){
-     this.registerForm = this.fb.group({
+     this.credentialsForm = this.fb.group({
       email: ['',[Validators.required,Validators.email]],
       displayName:['',Validators.required],
       password: ['',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]],
       confirmPassword :['',[Validators.required,this.matchValues('password')]]
     });
-    this.registerForm.controls['password'].valueChanges.subscribe(()=>{ 
-      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+    this.credentialsForm.controls['password'].valueChanges.subscribe(()=>{ 
+      this.credentialsForm.controls['confirmPassword'].updateValueAndValidity();
+    })
+
+    this.profileForm = this.fb.group({
+      gender: ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required]
     })
   }
 
@@ -42,8 +51,21 @@ export class Register{
     }
   }
 
+  nextStep(){
+    if(this.credentialsForm.valid){
+      this.currentStep.update(prevStep => prevStep + 1);
+    }
+  }
+
+  prevStep(){
+    this.currentStep.update(prevStep => prevStep - 1);
+  }
+
   register(){
-    console.log(this.registerForm.value);
+   if(this.profileForm.valid && this.credentialsForm.valid){
+    const formData = {...this.credentialsForm.value, ...this.profileForm.value};
+    console.log('Form data:', formData);
+   }
 
     //this.accountService.register(this.creds).subscribe({
      // next: response => {
