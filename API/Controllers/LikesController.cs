42d522aeb713f418d77ff52235d90ab485d2204e
amarcosic7 <amarcosic2016@gmail.com>
2026-09-01 19:06:02 +0200

    feat: add likes controller

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,54 @@
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class LikesController(ILikesRepository likesRepository) : BaseApiController
+{
+    [HttpPost("{targetMemberId}")]
+    public async Task<ActionResult> ToggleLike(string targetMemberId)
+    {
+        var sourceMemberId = User.GetMemberId();
+
+        if(sourceMemberId == targetMemberId) return BadRequest("You cannot like yourself");
+
+        var existingLike = await likesRepository.GetMemberLike(sourceMemberId,targetMemberId);
+
+        if(existingLike == null)
+        {
+            var like = new MemberLike
+            {
+                SourceMemberId = sourceMemberId,
+                TargetMemberId = targetMemberId
+            };  
+
+            likesRepository.AddLike(like);
+
+        }
+        else
+        {
+            likesRepository.DeleteLike(existingLike);
+        }
+
+        if(await likesRepository.SaveAllChanges()) return Ok();
+
+        return BadRequest("Failed to updadate like");
+    }
+
+    [HttpGet("list")]
+    public async Task<ActionResult<IEnumerable<string>>> GetCurrentMemberLikeIds()
+    {
+        return Ok(await likesRepository.GetCurrentMemberByIds(User.GetMemberId()));
+    }
+
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetMemberLikes(string predicate)
+    {
+        var members = await likesRepository.GetMemberLikes(predicate,User.GetMemberId());
+
+        return Ok(members);
+    }
+}
